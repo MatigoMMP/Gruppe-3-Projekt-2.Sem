261bf1c540b9e184b6721ba4909c3b3e1369fb71
victorproexp <82384771+victorproexp@users.noreply.github.com>
2021-11-29 12:00:59 +0100

    festival1.0

---
+++ miljoefestival/miljoefestival/Server/DataAccess/DomainModelPostgreSqlContext.cs
@@ -0,0 +1,25 @@
+﻿using miljoefestival.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace miljoefestival.Server.DataAccess
+{
+    public class DomainModelPostgreSqlContext : DbContext
+    {
+        public DomainModelPostgreSqlContext(DbContextOptions<DomainModelPostgreSqlContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> bruger { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public override int SaveChanges()
+        {
+            ChangeTracker.DetectChanges();
+            return base.SaveChanges();
+        }
+    }
+}
---
+++ miljoefestival/miljoefestival/Shared/Models/IDataAccessProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace miljoefestival.Shared.Models
+{
+    public interface IDataAccessProvider
+    {
+        void AddUserRecord(User user);
+        void UpdateUserRecord(User user);
+        void DeleteUserRecord(int id);
+        User GetUserSingleRecord(int id);
+        List<User> GetUserRecords();
+    }
+}
