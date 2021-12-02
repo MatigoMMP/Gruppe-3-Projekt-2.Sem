4b6678fe8b78977ef4c983648285c9599ead2abb
victorproexp <82384771+victorproexp@users.noreply.github.com>
2021-12-02 09:36:03 +0100

    festival - mongodb

---
+++ festival/festival/Client/Models/Employees.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace festival.Models
+{
+    public class Employee
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("first_name")]
+        public string First_name { get; set; }
+
+        [BsonElement("last_name")]
+        public string Last_name { get; set; }
+
+        [BsonElement("age")]
+        public decimal Age { get; set; }
+
+        [BsonElement("email")]
+        public string Email { get; set; }
+
+        [BsonElement("username")]
+        public string Username { get; set; }
+
+        [BsonElement("password")]
+        public string Password { get; set; }
+    }
+}
---
+++ festival/festival/Client/Models/FestDatabaseSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace festival.Models
+{
+    public class FestDatabaseSettings : IFestDatabaseSettings
+    {
+        public string EmployeesCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IFestDatabaseSettings
+    {
+        string EmployeesCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
